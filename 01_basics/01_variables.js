24e511f4b28683f747111786f05622110851435c
Adarsh Sharma <adarshkohli183@gmail.com>
2024-07-25 06:29:31 +0000

    done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,13 @@
+const accountId = 12121
+let accountEmail = "adarsh123@gmail.com"
+var accountPassword = "12345"
+
+accountCity = "Jaipur"
+
+// accountId = 23421
+accountEmail = "dvyue@gmail.com"
+accountPassword = "2321"
+accountCity = "Bengaluru"
+
+console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,accountCity])
